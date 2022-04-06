2d5ea0caab9cbd1e955d3d31afbac048b684c5b6
Kyle Metz <17kylemetz@gmail.com>
2022-04-06 15:35:07 -0400

    init commit
    This is my final project for Graphics Programming. I will be creating a FarmVille Clone using the OpenGL library OpenTK with C#

---
+++ FarmVilleClone/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FarmVilleClone
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
